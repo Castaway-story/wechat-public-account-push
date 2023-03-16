35ad1a3f2e7ab835402f5674593b911ba3a189ad
Castaway-story <65396460+Castaway-story@users.noreply.github.com>
2023-03-16 14:02:21 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'obffm6B6ybYQI-yiiioN_YKVwTiE',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'VPF_1EEsO-2Kzr7yXZwIwPKt0Qbdcw0wq6UoVLqf6iE',
+      useTemplateId: 'riV35a1Ps8fu5KuwFZcWesT5cjg57uH8BZb-9Xe7370',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-12',
       festivals: [
