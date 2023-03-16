26338770a453dffbe5bc67c27d2ca0c4277d5d89
Castaway-story <65396460+Castaway-story@users.noreply.github.com>
2023-03-16 14:37:42 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
 //       name: '早上好',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'obffm6B6ybYQI-yiiioN_YKVwTiE',
+      id: 'obffm6AWrpumgsmWu5YRJWCEzXhQ',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'pwqqOHZu2JvbV3eFTVydFA7cUPyEI3fu0RBeSQvl6K0',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
